cf7d28735a03cf909a4a6863f72f88268fa7efbb
Greepsu <arnaud.mgn2@gmail.com>
2021-08-11 15:18:09 +0200

    Change on GameContext

--- client/src/Contexts/GameContext.jsx
+++ client/src/Contexts/GameContext.jsx
@@ -1,78 +1,93 @@
-import React, { useState, useContext, createContext } from 'react';
+import React, { useState, useContext, createContext } from "react";
 
-import { generateRandomNumber } from "../Components/Helper"
+import { generateRandomNumber } from "../Components/Helper";
+
+import { ShifumiWeaponObject, ShifumiResultObject } from "../Enums/Shifumi";
 
 const GameContext = createContext({});
 
-export function ShifumiWeapon() {
-    const ShifumiWeaponObject = Object.freeze({
-        paper:"Paper",
-        scissors: "Scissors",
-        rock: "Rock"
-    })
-}
-
-export function ShifumiResult() {
-    const ShifumiResultObject = Object.freeze({
-        win:"Win",
-        loose:"Loose",
-        draw:"Draw"
-    })
-}
-
 export function GameContextProvider({ children }) {
-    const [userSelection, setUserSelection] = useState()
-    const [cpuSelection, setCpuSelection] = useState()
-    const [userMatchResult, setUserMatchResult] = useState()
+  const [userSelection, setUserSelection] = useState();
+  const [cpuSelection, setCpuSelection] = useState();
+  const [userMatchResult, setUserMatchResult] = useState();
+  const [score, setScore] = useState(0);
 
   //Set CPU choice
-  const randomCPUSelection = () => {
+  function randomCPUSelection() {
     const result = generateRandomNumber();
     switch (result) {
       case 0:
-        setCpuSelection("Rock");
+        setCpuSelection(ShifumiWeaponObject.ROCK);
         break;
       case 1:
-        setCpuSelection("Paper");
+        setCpuSelection(ShifumiWeaponObject.PAPER);
         break;
       case 2:
-        setCpuSelection("Scissors");
+        setCpuSelection(ShifumiWeaponObject.SCISSORS);
         break;
       default:
         console.log(`Sorry, Bot have some issues`);
     }
 
-        matchResult()
+    matchResult();
+  }
+  
+  //Set Match Result
+  const compareResult =
+    (userSelection === ShifumiWeaponObject.ROCK &&
+      cpuSelection === ShifumiWeaponObject.SCISSORS) ||
+    (userSelection === ShifumiWeaponObject.PAPER &&
+      cpuSelection === ShifumiWeaponObject.ROCK) ||
+    (userSelection === ShifumiWeaponObject.SCISSORS &&
+      cpuSelection === ShifumiWeaponObject.PAPER);
+
+  function matchResult() {
+    if (userSelection && cpuSelection) {
+      if (compareResult) {
+        setUserMatchResult(ShifumiResultObject.WIN);
+        setScore(score + 1);
+      } else if (userSelection === cpuSelection) {
+        setUserMatchResult(ShifumiResultObject.DRAW);
+      } else {
+        setUserMatchResult(ShifumiResultObject.LOOSE);
+        setScore(score - 1);
+      }
     }
+  }
 
-    //Set Match Result
-    const matchResult = () => {
-        if((userSelection === "Rock" && cpuSelection ==="Scissors") 
-        || (userSelection === "Paper" && cpuSelection ==="Rock") 
-        || (userSelection === "Scissors" && cpuSelection ==="Paper")) {
-            setUserMatchResult("You win !")
-        }else if(userSelection === cpuSelection){
-            setUserMatchResult("Draw !")
-        }else {
-            setUserMatchResult("You loose !")
-        }
-    }
+  function handleUserSelection(weapon) {
+    setUserSelection(weapon);
+  }
 
-    console.log(`${userSelection} vs ${cpuSelection} = ${userMatchResult}`)
+  function handleCpuSelection(weapon) {
+    setCpuSelection(weapon);
+  }
 
+  function handlePlayAgain() {
+    setUserSelection(undefined);
+    setCpuSelection(undefined);
+    setUserMatchResult(undefined);
+  }
 
-  const values = { userSelection, cpuSelection, userMatchResult };
+  console.log(`${userSelection} vs ${cpuSelection} = ${userMatchResult}`);
 
-  return (
-    <GameContext.Provider value={values}>{children}</GameContext.Provider>
-  );
+  const values = {
+    userSelection,
+    cpuSelection,
+    userMatchResult,
+    handleUserSelection,
+    handleCpuSelection,
+    handlePlayAgain,
+  };
+
+  return <GameContext.Provider value={values}>{children}</GameContext.Provider>;
 }
 
 export function useGameContext() {
   const context = useContext(GameContext);
   if (!context)
     throw new Error(
-      'useGameContext should be used within a GameContextProvider',
+      "useGameContext should be used within a GameContextProvider"
     );
   return context;
 }
---
+++ client/src/Enums/Shifumi.jsx
@@ -0,0 +1,11 @@
+const ShifumiWeaponObject = Object.freeze({
+    paper: "Paper",
+    scissors: "Scissors",
+    rock: "Rock",
+  });
+  
+  const ShifumiResultObject = Object.freeze({
+    win: "Win",
+    loose: "Loose",
+    draw: "Draw",
+  });
