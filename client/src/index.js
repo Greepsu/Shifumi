e9e4583f4558522b706101dc6029d2eb3f654d24
Greepsu <arnaud.mgn2@gmail.com>
2021-08-16 19:07:46 +0200

    Fix typo

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+//Import style
 import "./index.css";
+
+//Import App
 import App from "./Components/App.jsx";
 
+//Import COntexts
 import { GameContextProvider } from "../src/Contexts/GameContext";
 import { UserContextProvider } from "../src/Contexts/UserContext";
 import { WebSocketContextProvider } from "../src/Contexts/WebSocketContext";
