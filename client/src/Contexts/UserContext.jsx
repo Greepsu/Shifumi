1fa5606884c031dc0e0d41166b10a748cdd0e13b
Greepsu <arnaud.mgn2@gmail.com>
2021-08-19 20:40:48 +0200

    Implement login from front to back

--- client/src/Contexts/UserContext.jsx
+++ client/src/Contexts/UserContext.jsx
@@ -16,29 +16,21 @@ export function UserContextProvider({ children }) {
 
     console.log(`UserContext : user = ${user}`)
 
-    if (user === '') {
-      console.log("No user")
-    } else {
+    if (user)
       webSocket.on("connect", () => {
-        webSocket.emit('add user', user);
         console.log(`User ${user} connected on Front`);
       });
+    webSocket.emit('add user', user);
 
-      webSocket.on("weapon", (data) => {
-        console.log(data);
-      });
+    webSocket.on("weapon", (data) => {
+      console.log(data);
+    });
 
-      webSocket.emit("create", "room1");
+    webSocket.emit("create", "room1");
 
-      webSocket.on('login', (data) => {
-        const message = 'Welcome to Socket.IO Chat – ';
-        console.log(data);
-      });
-
-      return webSocket.on("disconnect", () => {
-        console.log(`User ${user} disconnected on Front`);
-      });
-    }
+    return webSocket.on("disconnect", () => {
+      console.log(`User ${user} disconnected on Front`);
+    });
   }, [webSocket, user]);
 
   // const sendWeapon = (weapon) => webSocket.emit("weapon", weapon);
--- server/index.js
+++ server/index.js
@@ -1,18 +1,18 @@
 //Import and use express
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const port = 5000;
 
 //Import CORS
-const cors = require('cors');
+const cors = require("cors");
 
 //Import Socket.io
-const server = require('http').Server(app);
-const io = require('socket.io')(server, {
+const server = require("http").Server(app);
+const io = require("socket.io")(server, {
   cors: {
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST'],
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
   },
 });
 
@@ -21,48 +21,37 @@ app.use(cors());
 app.use(express.json());
 
 //Socket.io
-let numUsers = 0;
 
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
+  socket.on("add user", (username) => {
+    socket.username = username;
 
-  console.log(`User with ${socket.id} ID connected on Back`);
-
-  socket.on('create', (room) => {
-    socket.join(room);
-    console.log(`User with ${socket.id} ID joining ${room}`);
-  });
-
-  socket.on('disconnect', () => {
-    console.log(`User with ${socket.id} ID disconnected on Back`);
-  });
-
-  socket.on('weapon', (data, room) => {
-    if(room) {
-      socket.broadcast.emit('weapon', data)
-    }
-  })
-
-    // when the client emits 'add user', this listens and executes
-    socket.on('add user', (username) => {  
-      // we store the username in the socket session for this client
-      socket.username = username;
-      ++numUsers;
-      socket.emit('login', {
-        numUsers: numUsers
-      });
-
-      // echo globally (all clients) that a person has connected
-      socket.broadcast.emit('user joined', {
-        username: socket.username,
-        numUsers: numUsers
-      });
-      console.log(username)
+    console.log(`User ${socket.username} connected on Back`);
+    
+    socket.broadcast.emit("user joined", {
+      username: socket.username,
     });
+  });
+
+  socket.on("create", (room) => {
+    socket.join(room);
+    console.log(`User ${socket.username} joining ${room}`);
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`User ${socket.username} disconnected on Back`);
+  });
+
+  socket.on("weapon", (data, room) => {
+    if (room) {
+      socket.broadcast.emit("weapon", data);
+    }
+  });
+
+  
+
+  
 });
-
-
-
-
 
 //Listen
 server.listen(5001, () => {
